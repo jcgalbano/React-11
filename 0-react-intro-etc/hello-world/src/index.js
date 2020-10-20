3b0ccd9afed1df61bf99b04499ae0529f191561b
Jan Carlo Albano <jcgalbano@gmail.com>
2020-10-20 16:40:06 +0800

    Add in slides and Rename session 1 to 0

--- 0-react-intro-etc/hello-world/src/index.js
+++ 0-react-intro-etc/hello-world/src/index.js
@@ -2,8 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 ReactDOM.render(
+  // What we want shown
   <h1>
     Hello World!
-  </h1>,
+  </h1>
+  ,
+  // Where do we want to show it
   document.getElementById('root')
 )
